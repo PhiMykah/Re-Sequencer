87758de4b79db2103dcb902ed2646bc8ecb50a53
Mykah Smith <mykahsmith21@gmail.com>
2025-10-01 11:44:51 -0400

    Updated Pyr<>Pur Substitution

--- resequencer/residue/base.py
+++ resequencer/residue/base.py
@@ -24,25 +24,25 @@ class NucleotideBase(Enum):
 
 # Maps nucleotide base indices to lists of atom names that are deleted for each base
 deleted_atoms: "dict[int, list[str]]" = {
-    0: ["N1", "C2", "N3", "C5", "C6", "N6", "N7"], # A 
-    1: ["N1", "C2", "N2", "N3", "C5", "C6", "O6", "N7"], # G
-    2: ["O2", "N3", "C4", "N4", "C5"], # C
-    3: ["O2", "N3", "C4", "O4", "C5"], # U
-    4: ["O2", "N3", "C4", "O4", "C5", "C7"], # T
+    0: ["N1", "C2", "N3", "C5", "C6", "N6", "N7"],  # A
+    1: ["N1", "C2", "N2", "N3", "C5", "C6", "O6", "N7"],  # G
+    2: ["O2", "N3", "C4", "N4", "C5"],  # C
+    3: ["O2", "N3", "C4", "O4", "C5"],  # U
+    4: ["O2", "N3", "C4", "O4", "C5", "C7"],  # T
 }
 
 # Maps purine atom names to their corresponding pyrimidine atom names.
 purine_to_pyrimidine: "dict[str, str]" = {
     "N9": "N1",
-    "C4": "C6",
-    "C8": "C2",
+    "C4": "C2",
+    "C8": "C6",
 }
 
 # Maps pyrimidine atom names to their corresponding purine atom names.
 pyrimidine_to_purine: "dict[str, str]" = {
     "N1": "N9",
-    "C6": "C4",
-    "C2": "C8",
+    "C6": "C8",
+    "C2": "C4",
 }
 
 # List of indices corresponding to purine bases (Adenine and Guanine).
